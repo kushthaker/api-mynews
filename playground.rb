27ad5c159c846442bcfa9313fafd10cb276bd788
Kush <kushthaker@gmail.com>
2015-08-26 19:21:55 -0400

    httparty installed and tested

---
+++ playground.rb
@@ -0,0 +1,5 @@
+require 'httparty'
+
+response = HTTParty.get('http://api.nytimes.com/svc/mostpopular/v2/mostviewed/all-sections/7?api-key=8adaa431139b3e23bdb45bbd057b7af8:15:72555388')
+
+puts response.body, response.code, response.message, response.headers.inspect
