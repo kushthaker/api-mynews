08210ebb2a562162861c9f79f44be1ecf3eefa9e
Kush <kushthaker@gmail.com>
2015-08-29 16:27:47 -0400

    added api routes, controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  root 'api#home'
+
+  get '/api/nytimes', to: 'api#nytimes'
+  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- playground.rb
+++ playground.rb
@@ -2,4 +2,4 @@ require 'httparty'
 
 response = HTTParty.get('http://api.nytimes.com/svc/mostpopular/v2/mostviewed/all-sections/7?api-key=8adaa431139b3e23bdb45bbd057b7af8:15:72555388')
 
-puts response.body, response.code, response.message, response.headers.inspect
+puts response.body
